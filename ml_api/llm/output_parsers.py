b3a51a5b7e1b2d8e38506555471e657405c9338b
adityarags <adityarags@gmail.com>
2024-11-09 18:17:55 +0530

    Output parsers added for JSON output from LLM

---
+++ ml_api/llm/output_parsers.py
@@ -0,0 +1,31 @@
+import typing_extensions as T 
+import enum
+
+class AgeGroup(enum.Enum):
+    UNDER18 = "under18"
+    _18TO25 = "18to25"
+    _26TO40 = "26to40"
+    _40PLUS = "40+"
+
+
+class TimePreference(enum.Enum):
+    MORNING = "morning"
+    AFTERNOON = "afternoon"
+    NIGHT = "night"
+    LATENIGHT = "late night"
+
+
+class GenderPreference(enum.Enum):
+    MALE = "male"
+    FEMALE = "female"
+    NONBINARY = "non binary"
+    ANY = "any"
+
+
+
+class RoomRecommendation(T.TypedDict):
+    ageGroup: AgeGroup
+    languagePreference: str
+    genre: str
+    timePreference: TimePreference
+    genderPreference: GenderPreference
