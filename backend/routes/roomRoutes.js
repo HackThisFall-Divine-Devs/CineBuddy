f7c581ad6c32562854e1c44a59f76010b7fc7402
SUNIDHI-JAIN125 <sunidhijain0403@gmail.com>
2024-11-09 09:29:10 +0530

    Added Search Functionality for the movies

--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Room = require('../models/roomModel');
 const {
     createRoom,
     viewAllPublicRooms,
@@ -23,5 +24,37 @@ router.post('/:roomId/request-join', authenticate, requestToJoinRoom);
 router.post('/:roomId/handle-request/:userId', authenticate, handleJoinRequest);
 router.get('/:roomId/join-requests', authenticate, viewJoinRequests);
 router.post('/:roomId/add-friend', authenticate, addFriendToRoom);
+
+
+// search the movies with date or movie name - 
+router.get('/search', authenticate, async (req, res) => {
+  const { movieName, date } = req.query;
+  const query = {};
+
+
+  if (movieName) {
+    query.movieName = { $regex: movieName, $options: 'i' }; 
+  }
+
+  
+  if (date) {
+    query.date = new Date(date);
+  }
+
+  try {
+    const rooms = await Room.find(query)
+      .populate('createdBy', 'username email') 
+      .populate('joinedMembers', 'username email'); 
+
+    res.status(200).json({ success: true, rooms });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Error fetching rooms' });
+  }
+});
+
+
+
+
 
 module.exports = router;
