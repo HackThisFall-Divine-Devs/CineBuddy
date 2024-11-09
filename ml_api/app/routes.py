7ff7de1bba592e3643cc8314c6aea0e6b50bc08e
adityarags <adityarags@gmail.com>
2024-11-10 00:43:35 +0530

    Matchmaking Endpoint added for ML API

--- ml_api/app/routes.py
+++ ml_api/app/routes.py
@@ -1,7 +1,16 @@
 from flask import current_app as app
-import llm.gemini_pipe
+from flask import jsonify
+from utils.room_raning import rank_rooms
+import traceback
 
 @app.route("/")
 def healthcheck():
     return {"status": "healthy"}, 200
 
+@app.route("/mlapi/matchmaking/<user_id>", methods = ["GET"])
+def matchmaking(user_id):
+    try:
+        all_rooms = rank_rooms(user_id = user_id)
+        return jsonify({"ranked_rooms": all_rooms}), 200
+    except:
+        return jsonify({"status": "fail", "traceback": f"{traceback.format_exc()}"}), 500
