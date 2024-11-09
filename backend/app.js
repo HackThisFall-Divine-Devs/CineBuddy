7d820bef8f6680066453105912ef5946bddc884f
SUNIDHI-JAIN125 <sunidhijain0403@gmail.com>
2024-11-09 13:21:10 +0530

    App.js  updated

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,27 @@
 require('dotenv').config();
+require('./room-cleaner/roomDissolve.js'); 
 const express = require('express');
 const admin = require('firebase-admin');
+const adminsdkAccount = require('./config/cine-buddy-firebase-adminsdk.json');
 const connectDB = require('./database/db.js');
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  }),
+  credential: admin.credential.cert(adminsdkAccount),
 });
 
 const app = express();
 app.use(express.json());
 connectDB();
 
+
+
 const userRoutes = require('./routes/userRoutes');
 const roomRoutes = require('./routes/roomRoutes');
 
+
 app.use('/api/users', userRoutes);
 app.use('/api/rooms', roomRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
